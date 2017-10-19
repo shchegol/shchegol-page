f7d22072ef29a2043e6a8e44c587ece6d3a8c328
Alexander Shchegol <zelenzoom@gmail.com>
2017-10-19 17:56:20 +0300

    start / card animation + mobile behavior

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -4,23 +4,33 @@ export default class MainLayout extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {cardSide : ""};
+        this.state = { cardSide: "" };
         this.flipCard = this.flipCard.bind(this);
     }
 
     flipCard() {
         let cardState = this.state.cardSide === "" ? "card_flip" : "";
         this.setState({
-            cardSide : cardState
+            cardSide: cardState
         })
     }
 
     render() {
         return (
             <div className="container">
-                <main className={`card ${this.state.cardSide}`} onClick={this.flipCard}>
+                <main className={ `card ${this.state.cardSide}` } onClick={ this.flipCard }>
                     <section className="card__front">
                         Front
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, aut consectetur culpa delectus
+                            distinctio ducimus fugit hic, illo libero minima molestiae optio placeat quisquam quos
+                            reiciendis rem totam velit voluptatem.
+                        </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, aut consectetur culpa delectus
+                            distinctio ducimus fugit hic, illo libero minima molestiae optio placeat quisquam quos
+                            reiciendis rem totam velit voluptatem.
+                        </p>
                     </section>
 
                     <section className="card__back">
